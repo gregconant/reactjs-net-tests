02461eb45fc4dc95c28d3cb4ff82b7d8cd2f54a1
Greg Conant <gconant@connexustechnology.com>
2015-03-16 13:32:06 -0400

    gc - initial commit with one react component. Following official reactjs.net tutorial for now.

---
+++ reactjs-net/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(reactjs_net.Startup))]
+namespace reactjs_net
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
